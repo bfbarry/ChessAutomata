ec93a1b62b228adc72c126e496860639d6a4bd09
bfbarry <barry.brian.f@gmail.com>
2023-09-25 21:18:01 -0700

    c++ shenanigans

--- chess/cpp_engine/game.cpp
+++ chess/cpp_engine/game.cpp
@@ -2,20 +2,42 @@
 #include <map>
 #include <array> 
 #include "pieces.h"
-using namespace std;
 
 
 static const char TEAMS[2] = {'w', 'b'};
-// static const std::map<char, std::string> COLOR_MAP = {}
 
 class Chess {
-    array<string, 6> piece_names = {"Pawn", "Rook", "Knight", "Bishop", "Queen", "King"};
-    array<Piece, 40> pieces;
+    std::array<Piece, 40> pieces;
+    std::map<char, int> check;
+    std::map<char, int> mate;
+    std::map<std::string, std::vector<int>> piece_to_col_positions;
+
     public:
         Chess() {
-            // can also do it this way if more complex https://stackoverflow.com/questions/4007382/how-to-create-class-objects-dynamically
-            for (auto i: piece_names) {
-                pieces
+            for (auto t: TEAMS) {
+                check[t] = false;
+                mate[t] = false;
             };
-        }
+            piece_to_col_positions["Pawn"] = {0,1,2,3,4,5,6,7};
+            piece_to_col_positions["Rook"] = {0,7};
+            piece_to_col_positions["Knight"] = {1,6};
+            piece_to_col_positions["Bishop"] = {2,5};
+            piece_to_col_positions["Queen"] = {2};
+            piece_to_col_positions["King"] = {4};
+            
+            // can also do it this way if more complex https://stackoverflow.com/questions/4007382/how-to-create-class-objects-dynamically
+            // for (auto pn: piece_names) {
+            //     int row_w;
+            //     int row_b;
+            //     if (pn == "Pawn") {
+            //         row_w = 1;
+            //         row_b = 6;
+            //     }
+            //     else {
+            //         row_w = 0;
+            //         row_b = 7;
+            //     }
+            //     pieces
+            // };
+        };
 };
